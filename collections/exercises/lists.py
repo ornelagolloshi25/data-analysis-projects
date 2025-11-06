04ff8c9f9112888833a02a45d6a87a4944f949df
Ornela Golloshi <ornelagolloshi25@gmail.com>
2025-11-06 17:27:05 -0500

    collections-exercies-list

--- collections/exercises/lists.py
+++ collections/exercises/lists.py
@@ -1,25 +1,30 @@
 # Create the adding_practice list with the following entry: 273.15
+adding_practice = [273.15]
 
 # Use the append method to add the number 42 and the string "hello" to the list. Add these new items one at a time.  Print the list after each step to confirm the changes.
+adding_practice.append(42)
+adding_practice.append("hello")
 
 # Use list concatenation to add these three items to the list all at once: [False, -4.6, '87'].
+adding_practice += [False, -4.6, '87']
 
 # Use the cargo_hold list for the next set of exercises.
 cargo_hold = ['oxygen tanks', 'space suits', 'parrot', 'instruction manual', 'meal packs', 'slinky', 'security blanket']
 
 # Use bracket notation to replace 'slinky' in the list with 'space tether'. Print the list to confirm the change.
-
+cargo_hold[5] = 'space tether'
 
 # Remove the last item from the list with pop. Print the element removed and the updated list.
-
+removed_last = cargo_hold.pop()
 
 # Remove the first item from the list with pop. Print the element removed and the updated list.
-
+removed_first = cargo_hold.pop(0)
 
 # append and insert require arguments inside the (). Add the items 1138 and ‘20 meters’ to the the list - the number at the start and the string at the end. Print the updated list to confirm the changes.
-
+cargo_hold.insert(0, 1138)
 
 # Use the remove method to take the parrot out of cargo_hold, then print the updated list.
-
+cargo_hold.remove('parrot')
 
 # Use .format() to print the final list and its length. "The list ___ contains ___ items."
+print("The list {} contains {} items.".format(cargo_hold, len(cargo_hold)))
